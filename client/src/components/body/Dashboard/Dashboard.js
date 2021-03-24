64b9d7d44a56af2219acd1d4c387be984c154f47
Munnazzah Aslam <aslammunnazzah@gmail.com>
2021-03-24 21:35:58 +0500

    forth commit

--- client/src/components/body/Dashboard/Dashboard.js
+++ client/src/components/body/Dashboard/Dashboard.js
@@ -12,32 +12,36 @@ import '../Dashboard/Dashboard.css';
 
 function Dashboard() {
     return (
-        <div style={{ color: '#1B2653'}} className="main">
+        <div style={{ color: '#1B2653' }} className="main">
             <div style={{ display: 'flex', justifyContent: 'center' }}>
-                <Card className="card1" style={{boxShadow: 'none'}}>
-                    <CardContent style={{color:'#fff'}}>
-                    <span style={{fontSize:'0.8em', fontWeight:'300', marginRight:'0.5rem',marginLeft:'0.5rem'}}>Vendors</span>21 
-                    <br/><Link to="/search/vendors" style={{color:'#fff', fontWeight:'400'}}><u>View All</u></Link>
+            <iframe class="airtable-embed" src="https://airtable.com/embed/shrHkYaVbBXJZ3Di5?backgroundColor=cyan" frameborder="0" onmousewheel="" width="150%" height="833" style={{background: 'transparent', border: '1px solid #ccc'}}></iframe> 
+            </div>
+            <div style={{ display: 'flex', justifyContent: 'center' }}>
+                <Card className="card1" style={{ boxShadow: 'none' }}>
+                    <CardContent style={{ color: '#fff' }}>
+                        <span style={{ fontSize: '0.8em', fontWeight: '300', marginRight: '0.5rem', marginLeft: '0.5rem' }}>Vendors</span>21
+                    <br /><Link to="/search/vendors" style={{ color: '#fff', fontWeight: '400' }}><u>View All</u></Link>
                     </CardContent>
                 </Card>
-            <Card  className="card2" style={{boxShadow: 'none'}}><CardContent style={{color:'#fff'}}>
-            <span style={{fontSize:'0.8em', fontWeight:'300', marginRight:'0.5rem'}}>Communication APIs</span>141 <br/><Link to="/search/apis" style={{color:'#fff', fontWeight:'400'}}><u>View All</u></Link>
-                   </CardContent></Card>
-            <Card  className="card3"  style={{boxShadow: 'none'}}><CardContent style={{color:'#fff'}}>
-            <span style={{fontSize:'0.8em', fontWeight:'300', marginRight:'0.5rem'}}>Use Cases</span>425 <br/><Link to="/search/usecases" style={{color:'#fff', fontWeight:'400'}}><u>View All</u></Link>
-                   </CardContent></Card>
-            <Card  className="card4"  style={{boxShadow: 'none'}}><CardContent style={{color:'#fff'}}>
-            <span style={{fontSize:'0.8em', fontWeight:'300', marginRight:'0.5rem'}}>Successes</span>342 <br/><Link to="/search/successes" style={{color:'#fff', fontWeight:'400'}}><u>View All</u></Link>
-                   </CardContent></Card>
-        </div>
-        <div style={{ display: 'flex', justifyContent: 'center' }}>
-            <Vendors/>
-            <APIs/>
-        </div>
-        <div style={{ display: 'flex', justifyContent: 'center'}}>
-            <Usecases/>
-            <SuccessStories/>
-        </div>
+                <Card className="card2" style={{ boxShadow: 'none' }}><CardContent style={{ color: '#fff' }}>
+                    <span style={{ fontSize: '0.8em', fontWeight: '300', marginRight: '0.5rem' }}>Communication APIs</span>141 <br /><Link to="/search/apis" style={{ color: '#fff', fontWeight: '400' }}><u>View All</u></Link>
+                </CardContent></Card>
+                <Card className="card3" style={{ boxShadow: 'none' }}><CardContent style={{ color: '#fff' }}>
+                    <span style={{ fontSize: '0.8em', fontWeight: '300', marginRight: '0.5rem' }}>Use Cases</span>425 <br /><Link to="/search/usecases" style={{ color: '#fff', fontWeight: '400' }}><u>View All</u></Link>
+                </CardContent></Card>
+                <Card className="card4" style={{ boxShadow: 'none' }}><CardContent style={{ color: '#fff' }}>
+                    <span style={{ fontSize: '0.8em', fontWeight: '300', marginRight: '0.5rem' }}>Successes</span>342 <br /><Link to="/search/successes" style={{ color: '#fff', fontWeight: '400' }}><u>View All</u></Link>
+                </CardContent></Card>
+            </div>
+            
+            <div style={{ display: 'flex', justifyContent: 'center' }}>
+                <Vendors />
+                <APIs />
+            </div>
+            <div style={{ display: 'flex', justifyContent: 'center' }}>
+                <Usecases />
+                <SuccessStories />
+            </div>
         </div >
     )
 }
