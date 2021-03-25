2525f564856a77095d669a405b5926e187534551
Munnazzah Aslam <aslammunnazzah@gmail.com>
2021-03-25 22:50:46 +0500

    forth commit

--- client/src/components/body/Dashboard/Dashboard.js
+++ client/src/components/body/Dashboard/Dashboard.js
@@ -12,11 +12,9 @@ import '../Dashboard/Dashboard.css';
 
 function Dashboard() {
     return (
-        <div style={{ color: '#1B2653' }} className="main">
-            <div style={{ display: 'flex', justifyContent: 'center' }}>
-                <iframe class="airtable-embed" src="https://airtable.com/embed/shrHkYaVbBXJZ3Di5?backgroundColor=cyan" frameborder="0" onmousewheel="" width="150%" height="833" style={{ background: 'transparent', border: '1px solid #ccc' }}></iframe>
-            </div>
-            <div style={{ display: 'flex', justifyContent: 'center' }}>
+        <div style={{ color: '#1B2653' }}>
+            <iframe className="airtable-embed" src="https://airtable.com/embed/shrHkYaVbBXJZ3Di5?backgroundColor=cyan" frameborder="0" onmousewheel="" width="100%" height="1200" style={{background: 'transparent', border: '1px solid #ccc',left:'0',right:'0', bottom:'0'}}></iframe> 
+           {/* <div style={{ display: 'flex', justifyContent: 'center' }}>
                 <Card className="card1" style={{ boxShadow: 'none' }}>
                     <CardContent style={{ color: '#fff' }}>
                         <span style={{ fontSize: '0.8em', fontWeight: '300', marginRight: '0.5rem', marginLeft: '0.5rem' }}>Vendors</span>21
@@ -33,17 +31,15 @@ function Dashboard() {
                     <span style={{ fontSize: '0.8em', fontWeight: '300', marginRight: '0.5rem' }}>Successes</span>342 <br /><Link to="/search/successes" style={{ color: '#fff', fontWeight: '400' }}><u>View All</u></Link>
                 </CardContent></Card>
             </div>
-
-            { /* <div style={{ display: 'flex', justifyContent: 'center' }}>
+            
+            <div style={{ display: 'flex', justifyContent: 'center' }}>
                 <Vendors />
                 <APIs />
             </div>
             <div style={{ display: 'flex', justifyContent: 'center' }}>
                 <Usecases />
                 <SuccessStories />
-            </div>
-
-    */}
+    </div>*/}
         </div >
     )
 }
