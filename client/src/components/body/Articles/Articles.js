cece9d44d0b0e40d81219ce6c4ab5c9c8370e0f4
Munnazzah Aslam <aslammunnazzah@gmail.com>
2021-03-26 19:38:12 +0500

    forth commit

--- client/src/components/body/Articles/Articles.js
+++ client/src/components/body/Articles/Articles.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import Axios from "axios";
+import {Button} from '../../../globalStyles';
 export default function Articles(props) {
   const [post, setPost] = useState(null);
 
@@ -12,9 +13,16 @@ export default function Articles(props) {
   }, [setPost]);
 
   return (
-    <div style={{margin:'2rem'}}>
+    <div>
       {post && (
         <div>
+          <div style={{backgroundColor:'#EDEFF9', width:'100%'}}><h1 style={{minWidth:'1000px',maxWidth:'1000px',color: '#1D217E', textAlign: 'center',fontWeight:'700',fontSize:'2rem',margin: '0 auto',paddingTop: '44px',paddingBottom:'44px'}}>{post.title.rendered}</h1>
+          <div style={{display:'flex', flexDirection:'row', justifyContent:'center', paddingBottom:'44px'}}>
+            <Button style={{background:'#FA6396', marginRight:'1rem'}}>Article</Button>
+            <Button style={{background:'#14C0A9', marginRight:'1rem'}}>{post.date}</Button>
+            <Button style={{background:'#4B65F9', marginRight:'1rem'}}>TransformX</Button>
+          </div>
+          </div>
           <div dangerouslySetInnerHTML={{ __html: post.content.rendered }} />
         </div>
       )}
